964ccfe3c5f5518b3aee44c24af5a3d7a44c8e4b
sg4455 <sg4455@nyu.edu>
2018-04-29 17:47:27 -0400

    Added logout page

--- ctip/routes.py
+++ ctip/routes.py
@@ -162,18 +162,20 @@ def favicon():
 @app.route('/login', methods=['GET', 'POST'])
 def login():
     if current_user.is_authenticated:
+        flash('You are already logged in', 'error')
         return redirect(url_for('root'))
     form = LoginForm()
     if request.method == 'POST' and form.validate_on_submit():
         user = User.objects(email=form.email.data).first()
         login_user(user, remember = form.remember_me.data)
-        flash('You are now logged in')
+        flash('You are now logged in', 'info')
         return redirect(url_for('root'))
     return render_template('login.html', title='Sign In', form=form)
 
 @app.route('/register', methods=['GET', 'POST'])
 def register():
     if current_user.is_authenticated:
+        flash('You are already logged in', 'error')
         return redirect(url_for('root'))
     form = RegistrationForm()
     if request.method == 'POST' and form.validate_on_submit():
@@ -196,7 +198,7 @@ def save_start():
 def save():
     location = request.args.get('location')
     loc_obj = Location(email = current_user.email, address = location).save()
-    flash("Location Saved")
+    flash("Location Saved", 'info')
     return redirect(url_for('view_saved_locations'))
 
 @login_required
@@ -215,7 +217,7 @@ def reset_password_request():
         print(user.email)
         if user:
             send_password_reset_email(user)
-            flash('Check your email for the instructions to reset your password')
+            flash('Check your email for the instructions to reset your password', 'info')
             return redirect(url_for('login'))
     return render_template('reset_password_request.html',
                            title='Reset Password', form=form)
@@ -230,7 +232,7 @@ def reset_password(token):
     if form.validate_on_submit():
         user.set_password(form.password.data)
         user.save()
-        flash('password changed successfully')
+        flash('password changed successfully', 'info')
         return redirect(url_for('login'))
     return render_template('reset_password.html', form=form)
         
@@ -254,5 +256,4 @@ def confirm_email(token):
 @login_required
 def logout():
     logout_user()
-    flash("You are now logged out")
-    return redirect(url_for('root'))
+    return render_template('Logout.html')
---
+++ main.py
@@ -0,0 +1,1 @@
+from ctip import app
